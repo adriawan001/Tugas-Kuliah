ba7f20009e5b21db405aa2d4f4a16c948b0673fe
ADRIAWAN <100124362+adriawan001@users.noreply.github.com>
2022-03-20 10:03:41 +0800

    Add files via upload

---
+++ type set.py
@@ -0,0 +1,16 @@
+#type data set 
+se = {"alim","arif","awal","adri"}
+for se in se:
+    print(se)
+#mengupdate 
+se = {"alim","arif","awal","adri"}
+#se[1]"andi"
+print(se)
+#menghapus 
+se = {"alim","arif","awal","adri"}
+se.remove("arif")
+print(se)
+#menambahkan
+se = {"alim","arif","awal","adri"}
+se.add("adriawan")
+print(se)
